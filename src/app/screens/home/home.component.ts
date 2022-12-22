363409c890b76f020879fb8bbe711030e4747fbe
Phuntsho Norbu Tenzin <phuntsho.tenzin@selise.ch>
2022-12-22 12:50:59 +0600

    fix(yueloki): remove unused code

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -18,31 +18,6 @@ export class HomeComponent implements OnInit {
   pauseOnHover = false;
   pauseOnFocus = false;
 
-  @ViewChild('carousel', { static: true }) carousel: NgbCarousel;
-
-  togglePaused() {
-    if (this.paused) {
-      this.carousel.cycle();
-    } else {
-      this.carousel.pause();
-    }
-    this.paused = !this.paused;
-  }
-
-  onSlide(slideEvent: NgbSlideEvent) {
-    if (
-      this.unpauseOnArrow &&
-      slideEvent.paused &&
-      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
-    ) {
-      this.togglePaused();
-    }
-    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
-      this.togglePaused();
-    }
-  }
-
-
   constructor(private meta: Meta, private title: Title) {}
 
   ngOnInit(): void {
